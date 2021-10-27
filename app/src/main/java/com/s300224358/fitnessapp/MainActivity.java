60365da0710633c17e28b31bb31163d835cde6e2
jaypeemz <93142908+jaypeemz@users.noreply.github.com>
2021-10-27 00:35:30 -0700

    added nav and theme

--- app/src/main/java/com/s300224358/fitnessapp/MainActivity.java
+++ app/src/main/java/com/s300224358/fitnessapp/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //to add the title and logo
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher_fitness);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+
+
         setContentView(R.layout.activity_main);
+
+        //initializing nav bar
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+
     }
 }
