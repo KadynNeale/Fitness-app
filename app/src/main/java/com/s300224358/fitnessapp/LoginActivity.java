d1e624fdb76aa8b88952e28316f8f0bf704253c1
jaypeemz <93142908+jaypeemz@users.noreply.github.com>
2021-10-28 00:16:50 -0700

    added complete nav(maybe add one more?)

---
+++ app/src/main/java/com/s300224358/fitnessapp/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.s300224358.fitnessapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        startActivity(new Intent(LoginActivity.this,MainActivity.class));
+    }
+}
