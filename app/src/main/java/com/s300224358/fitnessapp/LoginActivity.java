1ab92937805082a2f7d60e7dad4eccd7a8a5a7e4
jaypeemz <93142908+jaypeemz@users.noreply.github.com>
2021-10-28 00:31:00 -0700

    added login

--- app/src/main/java/com/s300224358/fitnessapp/LoginActivity.java
+++ app/src/main/java/com/s300224358/fitnessapp/LoginActivity.java
@@ -4,6 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -12,6 +17,29 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        startActivity(new Intent(LoginActivity.this,MainActivity.class));
+        Button login = (Button) findViewById(R.id.btnLogin);
+        Button register = (Button) findViewById(R.id.btnRegister);
+
+        EditText user = (EditText) findViewById(R.id.txtUsername);
+        EditText pass = (EditText) findViewById(R.id.txtPassword);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(TextUtils.isEmpty(user.getText().toString())|| TextUtils.isEmpty(pass.getText().toString())){
+                    Toast.makeText(LoginActivity.this, "A field is empty", Toast.LENGTH_SHORT).show();
+                }else
+                    //write login check here
+                    startActivity(new Intent(LoginActivity.this,MainActivity.class));
+
+            }
+        });
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //write registration code here
+            }
+        });
+
     }
 }
